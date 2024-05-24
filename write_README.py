b5fcfe3ca33af4f6a8b286b55ec45825e2cc1cbd
sh1ro06293 <shiro.06293@gmail.com>
2024-05-25 01:27:08 +0900

    add redme

---
+++ write_README.py
@@ -0,0 +1,22 @@
+import glob
+
+files = list(glob.glob("./abc*"))
+text = """
+# 問題リンクとフォルダの一覧
+
+### テンプレート
+```md
+##
+[問題]()
+[フォルダ]()
+```
+"""
+for i in range(len(files)):
+    question_files = files[i]
+
+    files_name = list(question_files.split(".\\"))[1]
+    name_cut = list(files_name.split("_"))
+    text += f"## {files_name}\n[問題](https://atcoder.jp/contests/{name_cut[0]}/tasks/{files_name})\n\n[フォルダ]({files_name})\n"
+
+    with open ("README.md", "w", encoding= "utf-8") as f:
+        f.write(text)
