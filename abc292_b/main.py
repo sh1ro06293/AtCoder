49645d277ccb7a35357d965ee1c6726483ec09ed
githubsh1ro06293 <githubshiro.06293@gmail.com>
2024-05-27 09:55:10 +0900

    feat: optimize code for better performance

--- abc292_b/main.py
+++ abc292_b/main.py
@@ -1,21 +1,37 @@
+# n, q = map(int, input().split())
+# event_list = list()
+# event =  dict()
+
+# for i in range(q):
+#     event_list.append(list(map(int, input().split())))
+
+# for i in range(q):
+#     if event_list[i][0] == 3:
+#         if event_list[i][1] not in event:
+#             print("No")
+#         else:
+#             if event[event_list[i][1]] == 1:
+#                 print("No")   
+#             else:
+#                 print("Yes")
+#     elif event_list[i][1] in event:
+#         if event[event_list[i][1]] == 1:
+#             event[event_list[i][1]] = 2
+#     else:
+#         event[event_list[i][1]] = event_list[i][0]
+
 n, q = map(int, input().split())
-event_list = list()
-event =  dict()
+dic = {}
+for i in range(n):
+    dic[i+1] = 0
 
 for i in range(q):
-    event_list.append(list(map(int, input().split())))
-
-for i in range(q):
-    if event_list[i][0] == 3:
-        if event_list[i][1] not in event:
-            print("No")
+    score, player = map(int, input().split())
+    if score == 3:
+        if dic[player] >= 2:
+            print('Yes')
         else:
-            if event[event_list[i][1]] == 1:
-                print("No")   
-            else:
-                print("Yes")
-    elif event_list[i][1] in event:
-        if event[event_list[i][1]] == 1:
-            event[event_list[i][1]] = 2
+            print('No')
     else:
-        event[event_list[i][1]] = event_list[i][0]
+        dic[player] += score
+    
