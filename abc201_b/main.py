0c845c35464a4b9047957e2ce78118182a06b407
githubsh1ro06293 <githubshiro.06293@gmail.com>
2024-05-31 12:13:53 +0900

    Optimize code for better performance

--- abc201_b/main.py
+++ abc201_b/main.py
@@ -4,4 +4,11 @@ d = dict()
 for i in range(n):
     s, t = map(str, input().split())
     d[s] = int(t)
+ds = list(d.keys())
+dv = list(d.values())
+dv.sort(reverse=True)
+
+for i in range(n):
+    if d[ds[i]] == dv[1]:
+        print(ds[i])
 
