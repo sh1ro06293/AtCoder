203ac6d6cbd83e7bcc1728aebf665a510ec19588
githubsh1ro06293 <githubshiro.06293@gmail.com>
2024-05-31 12:31:53 +0900

    Refactor code for swapping 6 and 9 in a string

--- abc202_b/main.py
+++ abc202_b/main.py
@@ -1,12 +1,24 @@
-s = list(map(int, input()))
-ans =""
+# s = list(map(int, input()))
+# ans =""
 
-for i in range(len(s)):
-    if s[i] == 6:
-        s[i] = 9
-    elif s[i] == 9:
-        s[i] = 6
+# for i in range(len(s)):
+#     if s[i] == 6:
+#         s[i] = 9
+#     elif s[i] == 9:
+#         s[i] = 6
     
-    ans = str(s[i]) + ans
+#     ans = str(s[i]) + ans
 
-print(ans)
+# print(ans)
+
+s = input()
+
+ans = ''
+for i in s:
+    if i == '6':
+        i = '9'
+    elif i == '9':
+        i = '6'
+    ans += i
+
+print(ans[::-1])
