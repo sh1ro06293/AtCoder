6d0bb1a3a19e8a8bae29f3e19a1a1dc8be2863db
sh1ro06293 <shiro.06293@gmail.com>
2024-06-04 21:54:22 +0900

    add 356-a

--- abc356_a/main.py
+++ abc356_a/main.py
@@ -1,10 +1,19 @@
-n , l , r = map(int, print().split())
+n , l , r = map(int, input().split())
 a = list()
-l=list()
+s=list()
+ans = list()
 for i in range(n):
     a.append(i+1)
 
-
 for i in range(n):
     if l-1 <= i <= r-1:
-        
+        s.append(a[i])
+    else:
+        ans.append(a[i])
+    if i == r-1:
+        sr = sorted(s, reverse=True)
+        for j in sr:
+            ans.append(j)
+
+
+print(*ans)
