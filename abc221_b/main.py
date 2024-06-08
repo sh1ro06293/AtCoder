985b7d68694a98a74c09e37a0b583fdebe6b62a9
sh1ro06293 <shiro.06293@gmail.com>
2024-06-08 11:32:51 +0900

    feat: Fix bug in main.py for abc221_b problem

--- abc221_b/main.py
+++ abc221_b/main.py
@@ -7,10 +7,12 @@ if (s[len(s)-2] == t[len(t)-2]) and (s[len(s)-1] != t[len(t)-1]):
 else:
     for i in range(len(s)-1):
         if s[i] != t[i]:
-            kari = s[i]
+            si = s[i]
             s[i] = s[i+1]
-            s[i+1] = s[i]
+            s[i+1] = si
             for j in range(len(s)-1):
-                if s[i] != t[i]:
+                if s[j] != t[j]:
                     ans = "No"
                     break
+
+print(ans)
