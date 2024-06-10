6bc0c5a705f36f44deb9fef013d8e9c56568ab09
githubsh1ro06293 <githubshiro.06293@gmail.com>
2024-06-10 23:37:22 +0900

    feat: Add code to convert input string to lowercase if the number of uppercase letters is less than or equal to the number of lowercase letters

--- abc357/abc357_b.py
+++ abc357/abc357_b.py
@@ -1,1 +1,13 @@
-s = list(input())
+x = input()
+s = list(x)
+o= 0
+
+for i in range(len(s)):
+    if s[i].isupper():
+        o += 1
+
+if o <= len(s)-o:
+    # 小文字の時
+    print(x.lower())
+else:
+    print(x.upper())
