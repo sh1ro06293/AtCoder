647d45c2adfcaf1d64a0ae6ca80811fe162a2b68
sh1ro06293 <shiro.06293@gmail.com>
2024-08-08 01:57:13 +0900

    Refactor abc337_b.py for improved readability and performance

--- practice/abc337/abc337_b.py
+++ practice/abc337/abc337_b.py
@@ -1,35 +1,37 @@
 s = list(input())
 ans = "Yes"
 
-if  (len(set(s)) == 1) and s[0] == "A" or s[0] == "B" or s[0] == "C":
+if  (len(set(s)) == 1) and (s[0] == "A" or s[0] == "B" or s[0] == "C"):
     pass
 else:
-    count = 0
+    if s[0] == "A":
+        count = 0
+    elif s[0] == "B":
+        count = 1
+    else:
+        count = 2
 
     for i in range(len(s)-1):
         if count == 0:
-            if s[i] != "A":
-                ans = "No"
             if  s[i + 1] == "A":
                 pass
             elif s[i + 1] == "B":
                 count += 1
             else:
-                ans = "No"
+                count += 2
 
-        if count == 1:
+        elif count == 1:
             if  s[i + 1] == "B":
                 pass
             elif s[i + 1] == "C":
                 count += 1
             else:
                 ans = "No"
-
-        if count == 2:
-            if  s[i + 1] == "C":
-                pass
-            else:
+            pass
+        elif count == 2:
+            if  s[i + 1] != "C":
                 ans = "No"
+
         if ans == "No":
             break
 
